7f7d844bef74b357d7f7ac5dc7500e7eb328ae4d
henrique-pignatari <65184453+henrique-pignatari@users.noreply.github.com>
2020-11-08 05:03:23 -0300

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require('express')
     ,app = express()
     ,path =  require('path')
     ,bodyParser = require('body-parser')
-    ,http = require('http');
+    ,http = require('http')
+    ,socketio = require('socket.io');
 
 app.set('clientPath', path.join(__dirname, '../client'));
 app.use(express.static(app.get('clientPath')));
@@ -22,6 +23,18 @@ const routes = function(app){
 
 routes(app);
 
-app.listen(3000, function(){
+const server = http.createServer(app);
+const sockets = socketio(server);
+
+sockets.on('connection', (socket) => {
+    const connectionId = socket.id;
+    console.log(`> Connected with id: ${connectionId}`);
+
+    socket.on('emit',(msg)=>{
+        console.log(msg)
+    })
+})
+
+server.listen(3000, function(){
     console.log(`Servidor rodando na porta 3000!!`);
 });
